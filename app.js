d5f3ca95af4baaab81ccc3c1985a95499a1792f7
Progeim111 <102576858+Progeim111@users.noreply.github.com>
2022-10-25 15:49:24 +0300

    Variables

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Hello World!")
+// variables
+// let
+let name = 'Rait Sööt'
+name = 'New name'
+
+// const
+const surname = 'Sööt'
+// surname = 'Sööd' - error
+
+// arrays
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+
+// objects
+const person = {
+    firstname: 'Rait',
+    surname: 'Sööt'
+}
+console.log(person)
+console.log(person.firstname)
+console.log(person.surname)
+person.department = 'ITA21'
+console.log(person)
