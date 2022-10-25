b403898af4a556e5b33b489e723a9701e62d2737
Progeim111 <102576858+Progeim111@users.noreply.github.com>
2022-10-25 15:52:28 +0300

    Data types

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
-// variables
-// let
+// data types
+// String
 let name = 'Rait Sööt'
-name = 'New name'
 
-// const
-const surname = 'Sööt'
-// surname = 'Sööd' - error
+console.log(name)
+console.log(typeof name)
+
+let goodMark = 5
+console.log(goodMark)
+console.log(typeof goodMark)
+
+let persent = 50.3
+console.log(persent)
+console.log(typeof persent)
+
+let isHere = true // false
+console.log(isHere)
+console.log(typeof isHere)
 
 // arrays
 const numbers = [1, 2, 3, 4, 5]
 console.log(numbers)
-numbers.push(6)
-console.log(numbers)
+console.log(typeof numbers)
+
 
 // objects
 const person = {
     firstname: 'Rait',
     surname: 'Sööt'
 }
-console.log(person)
-console.log(person.firstname)
-console.log(person.surname)
-person.department = 'ITA21'
-console.log(person)
+console.log(typeof person)
+
+// Null
+const airplane = null
+console.log(airplane)
+console.log(typeof airplane)
+
+// Symbol
+const sym = Symbol()
+console.log(sym)
+console.log(typeof sym)
+
+// undefined
+let test
+console.log(test)
+console.log(typeof test)
